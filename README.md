555d3e9b62abf995d89cbf4d428973fd47e46e8c
BAN <vkak006@gmail.com>
2021-07-19 15:14:28 +0900

    add mongoDB config

--- README.md
+++ README.md
@@ -2,6 +2,9 @@
 LS electric 파일럿 프로젝트 demo version 작성
 
 #Version
-* Spring boot : 2.4.8 (temp)
-* Maven : 4.0.0
+* Spring boot : 2.4.8 (temp, change to 2.4.2)
+* Maven : 4.0.0 (temp, change to 3.8.1)
 
+#Goal
+* HATEOAS 사용법 숙지  
+* Reactive api application 작성 요령 숙지
---
+++ src/main/java/ls/electric/demo/config/MongoDBConfig.java
@@ -0,0 +1,27 @@
+package ls.electric.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.MongoDatabaseFactory;
+import org.springframework.data.mongodb.core.convert.DbRefResolver;
+import org.springframework.data.mongodb.core.convert.DefaultDbRefResolver;
+import org.springframework.data.mongodb.core.convert.DefaultMongoTypeMapper;
+import org.springframework.data.mongodb.core.convert.MappingMongoConverter;
+import org.springframework.data.mongodb.core.mapping.MongoMappingContext;
+
+@Configuration
+public class MongoDBConfig {
+
+    // 데이터 저장시 "_class" 필드 저장되지 않도록 설정
+    @Bean
+    public MappingMongoConverter mappingMongoConverter(
+            MongoDatabaseFactory mongoDatabaseFactory,
+            MongoMappingContext mongoMappingContext
+    ){
+        DbRefResolver dbRefResolver = new DefaultDbRefResolver(mongoDatabaseFactory);
+        MappingMongoConverter converter = new MappingMongoConverter(dbRefResolver, mongoMappingContext);
+        converter.setTypeMapper(new DefaultMongoTypeMapper(null));
+
+        return converter;
+    }
+}
