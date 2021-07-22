403e9b97b20fd0903c8095b8ab800be4e43836cc
BAN <vkak006@gmail.com>
2021-07-22 09:08:50 +0900

    deploy .jar

--- src/main/java/ls/electric/demo/DemoApplication.java
+++ src/main/java/ls/electric/demo/DemoApplication.java
@@ -7,11 +7,11 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-public class DemoApplication extends SpringBootServletInitializer {
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application){
-		return application.sources(DemoApplication.class);
-	}
+public class DemoApplication {
+//	@Override
+//	protected SpringApplicationBuilder configure(SpringApplicationBuilder application){
+//		return application.sources(DemoApplication.class);
+//	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
