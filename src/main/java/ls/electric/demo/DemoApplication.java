223ac6b14eed74acb1be7cbf4298d3df9e1f9a80
BAN <vkak006@gmail.com>
2021-07-22 11:21:28 +0900

    update pom.xml

--- src/main/java/ls/electric/demo/DemoApplication.java
+++ src/main/java/ls/electric/demo/DemoApplication.java
@@ -4,14 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-public class DemoApplication {
-//	@Override
-//	protected SpringApplicationBuilder configure(SpringApplicationBuilder application){
-//		return application.sources(DemoApplication.class);
-//	}
+public class DemoApplication extends SpringBootServletInitializer {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application){
+		return application.sources(DemoApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
