4e8718a69ded80cd5b28cae607fa460be1f4f688
BAN <vkak006@gmail.com>
2021-07-28 15:43:33 +0900

    commented security

--- src/main/java/ls/electric/demo/config/security/WebSecurityConfig.java
+++ src/main/java/ls/electric/demo/config/security/WebSecurityConfig.java
@@ -1,44 +1,40 @@
-package ls.electric.demo.config.security;
-
-import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-
-@Configuration
-@EnableWebSecurity
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception{
-        http.csrf().disable().authorizeRequests()
-                .antMatchers("/sample").authenticated()
-                .antMatchers("/admin").hasRole("ADMIN")
-                .anyRequest().permitAll()
-                .and()
-                .formLogin()
-                .loginPage("/sample/login")
-                .successForwardUrl("/sample/login/success")
-                .failureForwardUrl("/sample/login/fail")
-                .permitAll()
-                .and();
-//        .addFilterBefore(customAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
-    }
-
-    // 정적자원 Security 설정하지않음
-    @Override
-    public void configure(WebSecurity web) throws Exception{
-        web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
-//        web.ignoring().antMatchers("/webjars/**","/swagger-resources","/swagger-ui.html","/swagger/**");
-    }
-
-    @Bean
-    public BCryptPasswordEncoder bCryptPasswordEncoder(){
-        return new BCryptPasswordEncoder();
-    }
-}
+//package ls.electric.demo.config.security;
+//
+//import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.builders.WebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//
+//@Configuration
+//@EnableWebSecurity
+//public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+//
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception{
+//        http.csrf().disable().authorizeRequests()
+//                .antMatchers("/admin").hasRole("ADMIN")
+//                .anyRequest().permitAll()
+//                .and()
+//            .formLogin()
+//                .loginPage("/login")
+//                .successForwardUrl("/login/success")
+//                .failureForwardUrl("/login/fail")
+//                .permitAll();
+////        .and().addFilterBefore(customAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
+//    }
+//
+//    // 정적자원 Security 설정하지않음
+//    @Override
+//    public void configure(WebSecurity web) throws Exception{
+//        web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+//    }
+//
+//    @Bean
+//    public BCryptPasswordEncoder bCryptPasswordEncoder(){
+//        return new BCryptPasswordEncoder();
+//    }
+//}
