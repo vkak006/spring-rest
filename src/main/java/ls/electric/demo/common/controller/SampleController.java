9589d835e5c15d051d70d4d6022ce4b2e23febd8
BAN <vkak006@gmail.com>
2021-07-28 14:15:55 +0900

    update spring-security

--- src/main/java/ls/electric/demo/common/controller/SampleController.java
+++ src/main/java/ls/electric/demo/common/controller/SampleController.java
@@ -9,8 +9,6 @@ import org.springframework.hateoas.EntityModel;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
-
 @RestController
 public class SampleController {
     @Autowired
@@ -20,4 +18,13 @@ public class SampleController {
     public EntityModel<Sample> test(){
         return entityToModelConverter.toModel(new Sample());
     }
+
+//    @GetMapping("/web-flux")
+//    public Mono<Sample> testWebFlux(){
+//        Sample sample = new Sample();
+//        sample.setName("test");
+//        sample.setPrefix("webFlux");
+//
+//        return Mono.just(sample);
+//    }
 }
--- src/main/java/ls/electric/demo/config/security/WebSecurityConfig.java
+++ src/main/java/ls/electric/demo/config/security/WebSecurityConfig.java
@@ -3,6 +3,7 @@ package ls.electric.demo.config.security;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -13,12 +14,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-    // 정적자원 Security 설정하지않음
-    @Override
-    public void configure(WebSecurity webSecurity){
-        webSecurity.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
-    }
-
     @Override
     protected void configure(HttpSecurity http) throws Exception{
         http.csrf().disable().authorizeRequests()
@@ -26,13 +21,20 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/admin").hasRole("ADMIN")
                 .anyRequest().permitAll()
                 .and()
-            .formLogin()
-                .loginPage("/index")
-                .successForwardUrl("/index")
-                .failureForwardUrl("/index")
+                .formLogin()
+                .loginPage("/sample/login")
+                .successForwardUrl("/sample/login/success")
+                .failureForwardUrl("/sample/login/fail")
                 .permitAll()
                 .and();
-                //.addFilterBefore(customAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
+//        .addFilterBefore(customAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
+    }
+
+    // 정적자원 Security 설정하지않음
+    @Override
+    public void configure(WebSecurity web) throws Exception{
+        web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+//        web.ignoring().antMatchers("/webjars/**","/swagger-resources","/swagger-ui.html","/swagger/**");
     }
 
     @Bean
