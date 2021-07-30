ecbfbd3ca25a731ae0152b410e4822ef2d288166
BAN <vkak006@gmail.com>
2021-07-30 15:40:42 +0900

    update README.md

--- README.md
+++ README.md
@@ -4,8 +4,17 @@ Spring boot 기반 RESTful API 파일럿 프로젝트 개발을 위한 스터디
 # Version
 * Spring boot : 2.4.8 (need change to 2.4.2)
 * Maven : 4.0.0 (need change to 3.8.1)
+* Swagger : 3.0.0
 
-# Goal
-* HATEOAS 사용법 숙지  
-* Reactive api application 작성 요령 숙지
-* Azure App Service 배포 테스트
+# API
+```
+# 정돈 후 내용 추가 예정
+```
+
+# Tech Stach
+* HATEOAS (netty FW 상에서 사용 가능한지 확인 필요)
+* WebFlux
+* Mongodb-reactive
+* Reactive-core
+* netty FW
+* Azure App Service 배포
--- src/main/java/ls/electric/demo/config/SwaggerConfig.java
+++ src/main/java/ls/electric/demo/config/SwaggerConfig.java
@@ -2,9 +2,6 @@ package ls.electric.demo.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-//import org.springframework.hateoas.client.LinkDiscoverer;
-//import org.springframework.hateoas.client.LinkDiscoverers;
-//import org.springframework.hateoas.mediatype.collectionjson.CollectionJsonLinkDiscoverer;
 import org.springframework.plugin.core.SimplePluginRegistry;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
@@ -12,6 +9,10 @@ import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+//HATEOAS import
+//import org.springframework.hateoas.client.LinkDiscoverer;
+//import org.springframework.hateoas.client.LinkDiscoverers;
+//import org.springframework.hateoas.mediatype.collectionjson.CollectionJsonLinkDiscoverer;
 
 import java.util.ArrayList;
 import java.util.List;
