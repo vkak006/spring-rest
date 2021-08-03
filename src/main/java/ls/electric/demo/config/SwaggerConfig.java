292cc0745f4492ae8e94f985b6069bdb37426de9
BAN <vkak006@gmail.com>
2021-08-03 09:26:42 +0900

    update SwaggerConfig.java

--- src/main/java/ls/electric/demo/config/SwaggerConfig.java
+++ src/main/java/ls/electric/demo/config/SwaggerConfig.java
@@ -2,7 +2,6 @@ package ls.electric.demo.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.plugin.core.SimplePluginRegistry;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -13,9 +12,6 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 //import org.springframework.hateoas.client.LinkDiscoverer;
 //import org.springframework.hateoas.client.LinkDiscoverers;
 //import org.springframework.hateoas.mediatype.collectionjson.CollectionJsonLinkDiscoverer;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Configuration
 @EnableSwagger2
@@ -30,11 +26,4 @@ public class SwaggerConfig {
                 .build()
                 .apiInfo(new ApiInfoBuilder().version("1.0").title("Organization API").description("Documentation Organization API v1.0").build());
     }
-
-//    @Bean
-//    public LinkDiscoverers discoverers(){
-//        List<LinkDiscoverer> plugins = new ArrayList<>();
-//        plugins.add(new CollectionJsonLinkDiscoverer());
-//        return new LinkDiscoverers(SimplePluginRegistry.create(plugins));
-//    }
 }
