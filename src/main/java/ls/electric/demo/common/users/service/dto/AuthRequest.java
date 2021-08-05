a960dc80df0e1c133072651810ab18fa41826588
BAN <vkak006@gmail.com>
2021-08-05 11:27:25 +0900

    add dto

---
+++ src/main/java/ls/electric/demo/common/users/service/dto/AuthRequest.java
@@ -0,0 +1,13 @@
+package ls.electric.demo.common.users.service.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthRequest {
+    private String name;
+    private String password;
+}
