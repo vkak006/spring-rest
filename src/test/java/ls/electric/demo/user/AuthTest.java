79d8a5fc057535692370d4cbac7e112129676e8f
BAN <vkak006@gmail.com>
2021-09-08 11:38:48 +0900

    add AES-128 Util Component

---
+++ src/test/java/ls/electric/demo/user/AuthTest.java
@@ -0,0 +1,24 @@
+package ls.electric.demo.user;
+
+import lombok.extern.slf4j.Slf4j;
+import ls.electric.demo.config.security.jwt.AesUtil;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@Slf4j
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AuthTest {
+
+    @Test
+    public void test() throws Exception {
+        AesUtil aesUtil = new AesUtil();
+        String enc = aesUtil.encrypt("password1234");
+        log.info(enc);
+
+        String dec = aesUtil.decrypt(enc);
+        log.info(dec);
+    }
+}
